2702c7ddc4c44705c632d1c0364dd52babc9cd82
jmm7gameover <jmm7gameover@gmail.com>
2024-07-15 13:22:46 -0600

    intentando solucinar el login ya que no funciona

--- js/functions.js
+++ js/functions.js
@@ -48,37 +48,3 @@ function assignEditEvents() {
   }
 
 
-  // login.js
-
-document.addEventListener('DOMContentLoaded', function () {
-  const loginForm = document.getElementById('loginForm');
-
-  loginForm.addEventListener('submit', async function (event) {
-      event.preventDefault();
-
-      const formData = new FormData(loginForm);
-      const email = formData.get('email');
-      const password = formData.get('password');
-
-      try {
-          const response = await fetch('/api/login', {
-              method: 'POST',
-              headers: {
-                  'Content-Type': 'application/json',
-              },
-              body: JSON.stringify({ email, password }),
-          });
-
-          if (!response.ok) {
-              throw new Error('Login failed');
-          }
-
-          // Redireccionar a otra página después del login exitoso
-          window.location.href = '/client/dashboard.html'; // Reemplaza con la ruta de tu dashboard o página principal
-
-      } catch (error) {
-          console.error('Error during login:', error);
-          // Aquí podrías mostrar un mensaje de error en el frontend si lo necesitas
-      }
-  });
-});
